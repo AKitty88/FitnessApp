6f4aeb84691f45a8a947319c6ae8bd5c32b71486
Kitti Almasy <a.kitty02@gmail.com>
2023-02-08 14:08:26 +1000

    Initial Commit

---
+++ Fitness/FitnessApp.swift
@@ -0,0 +1,17 @@
+//
+//  FitnessApp.swift
+//  Fitness
+//
+//  Created by User on 8/2/2023.
+//
+
+import SwiftUI
+
+@main
+struct FitnessApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
